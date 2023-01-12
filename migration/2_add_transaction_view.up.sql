215e0becfcf8c165d8dda6fb9a5ba7963cdac519
Hendra Danu Laksana <hdl.project.co@gmail.com>
2023-01-13 00:54:10 +0700

    feat: add transaction_query materialized view

---
+++ migration/2_add_transaction_view.up.sql
@@ -0,0 +1,14 @@
+CREATE MATERIALIZED VIEW IF NOT EXISTS transaction_query AS
+SELECT *
+FROM (SELECT transaction_id,
+             status,
+             amount,
+             product_code,
+             user_id,
+             created_at,
+             ROW_NUMBER() OVER (
+                 PARTITION BY transaction_id
+                 ORDER BY created_at DESC
+                 )
+      FROM transaction) transaction_temp
+WHERE row_number = 1;
